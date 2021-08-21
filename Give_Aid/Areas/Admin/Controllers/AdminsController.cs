06ffae2e117ac346a5c6b1bf91f3c19b3696b194
DESKTOP-77H711Q\tranq <anhdao25102001@gmail.com>
2021-08-21 21:44:41 +0700

    đạo

---
+++ Give_Aid/Areas/Admin/Controllers/AdminsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Give_Aid.Areas.Admin.Controllers
+{
+    public class AdminsController : Controller
+    {
+        // GET: Admin/Admins
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
