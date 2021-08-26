92b9691ba7fa49a993652355c9103d7f8d4086ec
DESKTOP-77H711Q\tranq <anhdao25102001@gmail.com>
2021-08-26 17:08:04 +0700

    dao

---
+++ Give_Aid/Areas/Admins/Controllers/CustomerController.cs
@@ -0,0 +1,98 @@
+﻿using Give_Aid.Common;
+using Give_Aid.Models.DAO;
+using Give_Aid.Models.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Give_Aid.Areas.Admins.Controllers
+{
+    public class CustomerController : BaseController
+    {
+        // GET: Admins/Customer
+        public ActionResult Index(string searchString, int page = 1, int pageSize = 7)
+        {
+            var dao = new CustomerDao();
+            var model = dao.GetAllPaging(searchString, page, pageSize);
+            ViewBag.SearchString = searchString;
+            return View(model);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Customer customer)
+        {
+
+            var dao = new CustomerDao();
+            var encryptedMd5Pas = Encryptor.MD5Hash(customer.PassWord);
+            customer.PassWord = encryptedMd5Pas;
+            int id = dao.Insert(customer);
+            if (id > 0)
+            {
+                SetAlert("Create Customer success", "success");
+                return RedirectToAction("Index", "Customer");
+            }
+            else
+            {
+                ModelState.AddModelError("", "error");
+            }
+
+            return View("Index");
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var adm = new CustomerDao().ViewDetail(id);
+            return View(adm);
+        }
+        [HttpPost]
+        public ActionResult Edit(Customer customer)
+        {
+
+            if (ModelState.IsValid)
+            {
+                var dao = new CustomerDao();
+                if (!string.IsNullOrEmpty(customer.PassWord))
+                {
+                    var encryptedMd5Pas = Encryptor.MD5Hash(customer.PassWord);
+                    customer.PassWord = encryptedMd5Pas;
+                }
+
+                var result = dao.Update(customer);
+                if (result)
+                {
+                    SetAlert("Update Customer success", "success");
+                    return RedirectToAction("Index", "Customer");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "error");
+                }
+            }
+
+            return View("Index");
+        }
+        [HttpDelete]
+        public ActionResult Delete(int id)
+        {
+            var dao = new CustomerDao();
+            var result = dao.Delete(id);
+            if (result)
+            {
+                SetAlert("Delete Customer success", "success");
+                return RedirectToAction("Index", "Customer");
+            }
+            else
+            {
+                ModelState.AddModelError("", "error");
+            }
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ Give_Aid/Models/DAO/CustomerDao.cs
@@ -0,0 +1,116 @@
+﻿using Give_Aid.Models.DataAccess;
+using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Give_Aid.Models.DAO
+{
+    public class CustomerDao
+    {
+        private NgoEntity db = null;
+
+        public CustomerDao()
+        {
+            db = new NgoEntity();
+        }
+
+        public int Insert(Customer entity)
+        {
+            db.Customers.Add(entity);
+            entity.CreateDate = DateTime.Now;
+            entity.UpdatedDate = DateTime.Now;
+            db.SaveChanges();
+            return entity.CustomerId;
+        }
+
+        public IEnumerable<Customer> GetAllPaging(string searchString, int page, int pageSize)
+        {
+            IQueryable<Customer> model = db.Customers;
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                model = model.Where(x => x.CustomerName.Contains(searchString)).OrderByDescending(x => x.CreateDate);
+            }
+            return model.OrderByDescending(x => x.CreateDate).ToPagedList(page, pageSize);
+        }
+
+        public bool Update(Customer customer)
+        {
+            try
+            {
+                var adm = db.Customers.Find(customer.CustomerId);
+                adm.CustomerName = customer.CustomerName;
+                adm.Email = customer.Email;
+                if (!string.IsNullOrEmpty(customer.PassWord))
+                {
+                    adm.PassWord = customer.PassWord;
+                }
+                adm.Address = customer.Address;
+                adm.Phone = customer.Phone;
+                adm.BirthDay = customer.BirthDay;
+                adm.CreateDate = customer.CreateDate;
+                adm.UpdatedDate = DateTime.Now;
+                adm.Status = customer.Status;
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public Customer GetbyId(string name)
+        {
+            return db.Customers.SingleOrDefault(x => x.CustomerName == name);
+        }
+
+        public Customer ViewDetail(int id)
+        {
+            return db.Customers.Where(a => a.CustomerId == id).FirstOrDefault();
+        }
+
+        //public bool Login(string name, string password)
+        //{
+        //    var result = db.Customers.SingleOrDefault(x => x.CustomerName == name);
+        //    if (result == null)
+        //    {
+        //        return Convert.ToBoolean(0);
+        //    }
+        //    else
+        //    {
+        //        if (result.Status == false)
+        //        {
+        //            return Convert.ToBoolean(-1);
+        //        }
+        //        else
+        //        {
+        //            if (result.PassWord == password)
+        //            {
+        //                return Convert.ToBoolean(1);
+        //            }
+        //            else
+        //            {
+        //                return Convert.ToBoolean(-2);
+        //            }
+        //        }
+        //    }
+        //}
+
+        public bool Delete(int id)
+        {
+            try
+            {
+                var Customer = db.Customers.Find(id);
+                db.Customers.Remove(Customer);
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
