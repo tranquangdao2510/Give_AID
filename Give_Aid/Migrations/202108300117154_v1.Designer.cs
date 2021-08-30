c05896656d01b3b8e4a1e6e15398f125c8f80ab7
Dang Nguyen <nguyenhaidang25072001@gmail.com>
2021-08-30 08:21:27 +0700

    dang

--- Give_Aid/Migrations/202108300117154_v1.Designer.cs
+++ Give_Aid/Migrations/202108300117154_v1.Designer.cs
@@ -7,13 +7,13 @@ namespace Give_Aid.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class v3 : IMigrationMetadata
+    public sealed partial class v1 : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(v3));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(v1));
         
         string IMigrationMetadata.Id
         {
-            get { return "202108270902565_v3"; }
+            get { return "202108300117154_v1"; }
         }
         
         string IMigrationMetadata.Source
