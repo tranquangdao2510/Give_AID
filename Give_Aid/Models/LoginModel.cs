52c2edb8f93db74744984f3bb4bc3f6c38442da4
DESKTOP-77H711Q\tranq <anhdao25102001@gmail.com>
2021-09-05 20:55:00 +0700

    Dao

---
+++ Give_Aid/Models/LoginModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Give_Aid.Models
+{
+    public class LoginModel
+    {
+        [Key]
+        [Required(ErrorMessage = "Please enter your sign-in name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Please enter your password")]
+        public string Password { get; set; }
+    }
+}
